497e399c7ae2e28a83131e9ff9f627f582734ceb
vbhasker <vinodh.ab@hotmail.com>
2024-08-23 21:04:03 -0400

    removed a typescript rule from the eslint.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello World, Vinodh!
+          Edit the <code>src/App.js</code> Hello World, Vinodh!
         </p>
       </header>
     </div>
